410b54e6357fea64e041e8f751c9772a6b5963d4
Colin Santee <santeedev@gmail.com>
2024-10-14 14:33:32 -0500

    Add Privacy Policy (#5)
    * Add Privacy Policy
    
    * Center footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,6 +14,25 @@ export default function Home() {
         />
         <span>Pool Scores</span>
       </main>
+      <footer className="bg-white rounded-lg shadow m-4 dark:bg-neutral-900 sticky top-[100vh]">
+        <div className="w-full mx-auto max-w-screen-xl p-4">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            <a href="https://colinjsantee.com/" className="hover:underline">
+              Colin Santee
+            </a>
+            . All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 justify-center">
+            <li>
+              <Link href="/privacy" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      
     </div>
   );
 }
